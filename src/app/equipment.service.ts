c0f0ed44f50d32830981f4ef347e7300ec2002af
luis951 <luishrt@hotmail.com>
2023-10-22 12:18:40 -0300

    changed server endpoints to aws

--- src/app/equipment.service.ts
+++ src/app/equipment.service.ts
@@ -9,18 +9,18 @@ export class EquipmentService {
   constructor(private http: HttpClient) { }
 
   getAllEquipment() {
-    return this.http.get('http://localhost:3000/equipment');
+    return this.http.get('http://54.232.47.135:3000/equipment');
   }
 
   createEquipment(equipment: { [key: string]: any }) {
-    return this.http.post('http://localhost:3000/equipment', equipment);
+    return this.http.post('http://54.232.47.135:3000/equipment', equipment);
   }
 
   updateEquipment(id: string, equipment: { [key: string]: any }) {
-    return this.http.patch('http://localhost:3000/equipment/' + id, equipment);
+    return this.http.patch('http://54.232.47.135:3000/equipment/' + id, equipment);
   }
 
   deleteEquipment(id: string) {
-    return this.http.delete('http://localhost:3000/equipment/' + id);
+    return this.http.delete('http://54.232.47.135:3000/equipment/' + id);
   }
 }
--- src/app/point.service.ts
+++ src/app/point.service.ts
@@ -9,18 +9,18 @@ export class PointService {
   constructor(private http: HttpClient) { }
 
   getAllPoint(equipment_id: string) {
-    return this.http.get('http://localhost:3000/point/byEquipment/' + equipment_id);
+    return this.http.get('http://54.232.47.135:3000/point/byEquipment/' + equipment_id);
   }
 
   createPoint(point: { [key: string]: any }) {
-    return this.http.post('http://localhost:3000/point', point);
+    return this.http.post('http://54.232.47.135:3000/point', point);
   }
 
   updatePoint(id: string, point: { [key: string]: any }) {
-    return this.http.patch('http://localhost:3000/point/' + id, point);
+    return this.http.patch('http://54.232.47.135:3000/point/' + id, point);
   }
 
   deletePoint(id: string) {
-    return this.http.delete('http://localhost:3000/point/' + id);
+    return this.http.delete('http://54.232.47.135:3000/point/' + id);
   }
 }
